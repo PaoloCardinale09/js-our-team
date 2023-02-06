7b5f423ab75d6d77c67c9466edcc39b8b6f09468
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-02-06 16:51:11 +0100

    add MILESTONE 10+ MILESTONE 1

--- js/main.js
+++ js/main.js
@@ -17,3 +17,40 @@
 // Angela Lopez		Social Media Manager	angela-lopez-social-media-manager.jpg
 // Scott Estrada		Developer				scott-estrada-developer.jpg
 // Barbara Ramos		Graphic Designer		barbara-ramos-graphic-designer.jpg
+
+// Creare l’array di oggetti con le informazioni fornite.
+const teamMembers = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    picture: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    picture: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    picture: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    picture: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    picture: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    picture: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+console.log(teamMembers);
+// console.log(teamMembers[0].role);
